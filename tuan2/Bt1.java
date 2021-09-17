f469861293a9a421dd5561ac4f630a649d11eaaa
ThanhTung19431261 <danhnam123569@gmail.com>
2021-09-17 17:10:08 +0700

    java co ban bai 1

--- tuan2/Bt1.java
+++ tuan2/Bt1.java
@@ -1,5 +1,9 @@
 package tuan2;
 
 public class Bt1 {
+    public static void main(String[] args) {
+        String s = new String("doi chu thuong thanh chu hoa");
+        System.out.println(s.toUpperCase());
+    }
     
 }
