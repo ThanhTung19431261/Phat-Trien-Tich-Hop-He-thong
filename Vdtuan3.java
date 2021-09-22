728ea1974a7094a039765c729eddebee567bf6a0
ThanhTung19431261 <danhnam123569@gmail.com>
2021-09-22 16:56:47 +0700

    Ví dụ tuần 3

---
+++ Vdtuan3.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Vdtuan3 implements Runnable {
+    String message;
+
+    Vdtuan3(String s) {
+        message = s;
+    }
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " Start msg: " + message);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " END");
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 10; i++) {
+            Runnable w = new Vdtuan3(s)
+        }
+    }
+}
