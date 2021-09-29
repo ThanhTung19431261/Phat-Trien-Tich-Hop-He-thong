163c56c44c79a81b849791a746530fd2a9d1e248
ThanhTung19431261 <danhnam123569@gmail.com>
2021-09-29 16:18:26 +0700

    Bt tuần 4 TCP

---
+++ TCPSocket/TCPEchoClient.java
@@ -0,0 +1,34 @@
+package TCPSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TCPEchoClient {
+    public final static String svIP = "127.0.0.1";
+    public final static int svPort = 12;
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Socket s= null;
+        try {
+            s = new Socket(svIP, svPort);
+            System.out.println("Client đã được tạo");
+            InputStream is = s.getOutputStream();
+            OutputStream os = s.getOutputStream();
+            for (int i = 0; i <= 9; i++) {
+                os.write(i);
+                int ch = is.read();
+                System.out.println(char(ch));
+                Thread.sleep(2000);
+            }
+        } catch (IOException ie) {
+            //TODO: handle exception
+            System.out.println("Không thể tạo socket");
+        } finally {
+            if(s != null){
+                s.close();
+            }
+        }
+    }
+}
---
+++ TCPSocket/TCPEchoServer.java
@@ -0,0 +1,41 @@
+package TCPSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPEchoServer {
+    public final static int svPort = 12;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(svPort);
+            System.out.println("Server đã được tạo");
+            while (true) {
+                try {
+                    Socket s = ss.accept();
+                    InputStream is = s.getInputStream();
+                    OutputStream os = s.getOutputStream();
+                    int ch = 0;
+                    while (true {
+                        ch = is.read();
+                        if (ch == -1) {
+                            break;
+                        }
+                        System.out.println(char(ch));
+                        os.write(ch);
+                    }
+                    s.close();
+                } catch (IOException ie1) {
+                    //TODO: handle exception
+                    System.out.println("Lỗi kết nối: " + ie1);
+                }
+            }
+        } catch (IOException ie) {
+            //TODO: handle exception
+            System.out.println("Không thể tạo server: " + ie);
+        }
+    }
+}
